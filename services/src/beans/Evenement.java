1c2ca2626f06b13dd594152b3373b96e73db2214
igaMobile <iga.mobile.2015@gmail.com>
2015-05-11 19:35:29 +0100

    initialisation du projet

---
+++ services/src/beans/Evenement.java
@@ -0,0 +1,23 @@
+package beans;
+
+import java.util.Date;
+
+public class Evenement {
+
+	private Date date;
+	private String description;
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
